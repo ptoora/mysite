274536bedde19a883a4a372d61f3dd16cc1aab28
Parsh <parsh.toora@theculturetrip.com>
2018-10-20 16:49:27 +0100

    Add stuff

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+const LAYOUT_TITLE = 'Parsh\'s Testing Strategy Wiki'
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express hello' });
+router.get('/', function(req, res, next) {
+ 
+  res.render('index', { title: LAYOUT_TITLE});
+
 });
 
-/* GET home page. */
-router.get('/short', function(req, res, next) {
-  res.render('index', { title: 'Express hello' + req.baseUrl });
+/* */
+router.get('/shorts', function(req, res, next) {
+ 
+  var files = fs.readdirSync('./public/shorts/');
+  res.render('shorts', { layoutTitle: LAYOUT_TITLE, title: 'Bite Size Blogs', shortArticles: files });
+
+});
+
+/* */
+router.get('/longs', function(req, res, next) {
+ 
+  var files = fs.readdirSync('./public/documents/');
+  res.render('longs', { layoutTitle: LAYOUT_TITLE, title: 'Long Blogs And Documents', shortArticles: files });
+
 });
 
 module.exports = router;
