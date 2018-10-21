ea55de66219fdc8b938e56aab5f1493d408d1a11
Parsh <parsh.toora@theculturetrip.com>
2018-10-21 18:04:32 +0100

    add

---
+++ public/javascripts/start.js
@@ -0,0 +1,34 @@
+$(document).ready(
+    function(){
+      $(".openableTitle1").click(function(){
+        if ($(".openable1").css("display")=="none")
+          $(".openable1").show();
+        else 
+        $(".openable1").hide();
+      })
+    }
+);
+
+$(document).ready(
+  function(){
+    $(".openableTitle2").click(function(){
+      if ($(".openable2").css("display")=="none")
+        $(".openable2").show();
+      else 
+      $(".openable2").hide();
+    })
+  }
+);
+
+$(document).ready(
+  function(){
+    $(".openableTitle3").click(function(){
+      if ($(".openable3").css("display")=="none")
+        $(".openable3").show();
+      else 
+      $(".openable3").hide();
+    })
+  }
+);
+
+    
