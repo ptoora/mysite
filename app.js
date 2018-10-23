0270caf864d151903f96b7a4fa7e6e747aace6a4
Parsh <parsh.toora@theculturetrip.com>
2018-10-23 14:14:01 +0100

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
